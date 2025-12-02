3b4e0f55590578a4914c2a5254b8d2ddce615461
yahyacoder7 <yahya.exe07@gmail.com>
2025-12-01 21:45:09 -0800

    git status

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const fs = require('fs').promises
 const port = 3000;
 
 
-app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, ".." , "public")))
 app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 })
 app.post('/', async (req, res) => {
     try {
